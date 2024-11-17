76b6d777a1712705ee50c8490373c5efa6973a28
ABDUL RAHIM <abdulrahim74264@zohomail.in>
2024-11-18 00:59:45 +0530

    Types of Interface Added

---
+++ ep-implicit-interface/explicit-interface.vb
@@ -0,0 +1,11 @@
+﻿Public Interface IExplicit
+    Sub Display()
+End Interface
+
+Public Class Base
+    Implements IExplicit
+
+    Private Sub Display() Implements IExplicit.Display
+        Console.WriteLine("This is Explicit Interface Example")
+    End Sub
+End Class
---
+++ ep-implicit-interface/implicite-interface.vb
@@ -0,0 +1,10 @@
+﻿Public Interface IExample
+    Sub Display()
+End Interface
+Public Class Test
+    Implements IExample
+
+    Public Sub Display() Implements IExample.Display
+        Console.WriteLine("This is Example of Implicit Interface")
+    End Sub
+End Class
