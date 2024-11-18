0675da10ce056eb0d0aa223439247629f6447aca
ABDUL RAHIM <abdulrahim74264@zohomail.in>
2024-11-18 20:30:53 +0530

    Helper and Owned Form

---
+++ ep-Helper-Owned-Forms/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim helper As New HelperForm
+        helper.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim owned As New OwnedForm
+        owned.Owner = Me
+        owned.Show()
+    End Sub
+End Class
---
+++ ep-Helper-Owned-Forms/OwnedForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class OwnedForm
+
+End Class
